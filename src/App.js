92ea1b905ad36e287667e3e199b9f7d83a2b4c0a
Yashwant_Sahu <yashwantsahu3002@gmail.com>
2022-03-30 17:33:21 +0530

    eCommerce Project in tree them is started.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+import EntryPoints from './components/EntryPoints';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {Auth} from './context/context'
 
 function App() {
+
+  // states for the authentication
+  const [auth, setAuth] = useState(null);
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <>
+          <BrowserRouter>
+          <Auth.Provider value={{auth,setAuth}}>
+            <Routes>
+                <Route path="/login" element={<EntryPoints/>} />
+                <Route path="/register" element={<EntryPoints/>} />
+            </Routes>
+          </Auth.Provider>
+         </BrowserRouter>
+   
+     </>
   );
 }
 
---
+++ src/components/EntryPoints.js
@@ -0,0 +1,217 @@
+import React,{useContext} from 'react'
+import {Button,Container,MenuItem,TextField,Typography,Grid,Box} from '@mui/material';
+import loginPoster from '../assets/img/login.jpeg';
+import signupPoster from '../assets/img/signup.jpeg';
+import  '../assets/custom/css/Login.css'
+import { GoogleLogin } from 'react-google-login';
+import FacebookLogin from 'react-facebook-login';
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import {Link} from 'react-router-dom';
+
+
+
+export default function EntryPoints() {
+
+  const handelButton  = ()=>{
+    console.log('Button Clicked')
+  }
+
+  const post = [
+    {
+      value: 'Admin',
+      label: 'Admin',
+    },
+    {
+      value: 'Accountant',
+      label: 'Accountant',
+    },
+    {
+      value: 'CEO',
+      label: 'CEO',
+    },
+    {
+      value: 'Driver',
+      label: 'Driver',
+    },
+    {
+      value: 'Delivery Person',
+      label: 'Delivery Person',
+    },
+    {
+      value: 'Manager',
+      label: 'Manager',
+    },
+    {
+      value: 'Security Gaurd',
+      label: 'Security Gaurd',
+    }
+  ];
+  
+  const [currency, setCurrency] = React.useState('EUR');
+
+  const handleChange = (event) => {
+    setCurrency(event.target.value);
+  };
+
+  return (
+    <>
+
+{/* // login module */}
+    {window.location.pathname === '/login' && 
+          <Container fixed sx={{height : 'content-fit'}} >
+              <Box p ={15} pt = {3} pb = {3}>
+                <Grid container sx={{borderRadius: 2,boxShadow : 3,overflow : 'hidden' }}  >
+                  {/* Image Side  */}
+                  <Grid   item id = 'imgBox' sm = {12} md = {6} lg = {6}  >
+                          <img style = {{width: '100%',height : '100%'}} src={loginPoster} alt="Login Poster" />
+                  </Grid>
+
+                  {/* Form Side */}
+                  <Grid  item p={6} sm = {12} md = {6} lg = {6}>
+                    <Typography  variant='h4'>
+                      Login
+                    </Typography>
+                    <form  method = 'post' className = 'formStyle'>
+                    <Box
+                      sx={{
+                        '& .MuiTextField-root': { mt: 2}, margin : 'auto'
+                      }}
+
+                      
+                    >
+
+                        
+                        <TextField  fullWidth autoComplete = {false}  id="fullWidth" label="Email" type = 'email' variant="outlined" />
+                        
+                        <TextField  fullWidth id="fullWidth"  type = 'password' label="Password" variant="outlined"  />
+
+                    
+                        <Button variant = 'contained' color = 'success' sx = {{width : '100%',marginTop : '5%',marginBottom : '8%',backgroundColor : '#0e9f6e'}}  > Log In</Button>
+                    </Box>
+                    </form>
+
+                    <hr/>
+
+
+                    <FacebookLogin
+                    appId="1088597931155576"
+                    autoLoad={true}
+                    fields="name,email,picture"
+                    // onClick={componentClicked}
+                    // callback={responseFacebook}
+                    textButton = {<Button  className = 'FacebookButton' startIcon = {<FacebookOutlinedIcon  />} >
+                    LOGIN WITH Facebook</Button>}
+                    cssClass = 'fbButton'
+                    / >
+                      
+
+                    <GoogleLogin
+                      clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+                      buttonText="LOGIN WITH GOOGLE"
+                      // onSuccess={responseGoogle}
+                      // onFailure={responseGoogle}
+                      cookiePolicy={'single_host_origin'}
+                      className = 'goggleButton'
+                      
+                    />
+
+                  <Typography variant = 'caption' sx = {{color : '#0e9f6e',display : 'block', marginTop : '8%'}} >Forgot Password?</Typography>  
+                
+
+                  <Typography variant = 'caption'>
+                  <Link className = 'links' to = '/register' >I do not have an account. Create Account  
+                    </Link></Typography>  
+
+                  </Grid>
+
+                </Grid>
+              </Box>
+          </Container>
+  }
+
+{/* // sign up module */}
+    {window.location.pathname === '/register' && 
+          <Container fixed sx={{height : 'content-fit'}} >
+              <Box p ={15} pt = {3} pb = {3}>
+                <Grid container sx={{borderRadius: 2,boxShadow : 3,overflow : 'hidden' }}  >
+                  {/* Image Side  */}
+                  <Grid   item id = 'imgBox' sm = {12} md = {6} lg = {6}  >
+                          <img style = {{width: '100%',height : '100%'}} src={signupPoster} alt="Login Poster" />
+                  </Grid>
+
+                  {/* Form Side */}
+                  <Grid  item p={6} sm = {12} md = {6} lg = {6}>
+                    <Typography  variant='h4'>
+                      Create an account
+                    </Typography>
+                    <form  method = 'post' className = 'formStyle'>
+                    <Box
+                      sx={{
+                        '& .MuiTextField-root': { mt: 2}, margin : 'auto'
+                      }}>
+
+                        
+                        <TextField  fullWidth autoComplete = {false}  id="fullWidth" label="Name" type = 'text' variant="outlined" />
+                        
+                        <TextField  fullWidth autoComplete = {false}  id="fullWidth" label="Email" type = 'email' variant="outlined" />
+                        
+                        <TextField  fullWidth id="fullWidth"  type = 'password' label="Password" variant="outlined"  />
+
+                        <TextField fullWidth
+                        id="outlined-select"
+                        select
+                        label="Select"
+                        value={currency}
+                        onChange={handleChange}
+                        helperText="Please select your post"
+                      >
+                        {post.map((option) => (
+                          <MenuItem key={option.value} value={option.value}>
+                            {option.label}
+                          </MenuItem>
+                        ))}
+                      </TextField>
+
+                    
+                        <Button variant = 'contained' color = 'success' sx = {{width : '100%',marginTop : '5%',marginBottom : '8%',backgroundColor : '#0e9f6e'}}  > Log In</Button>
+                    </Box>
+                    </form>
+
+                    <hr/>
+
+
+                    <FacebookLogin
+                    appId="1088597931155576"
+                    autoLoad={true}
+                    fields="name,email,picture"
+                    // onClick={componentClicked}
+                    // callback={responseFacebook}
+                    textButton = {<Button  className = 'FacebookButton' startIcon = {<FacebookOutlinedIcon  />} >
+                    LOGIN WITH Facebook</Button>}
+                    cssClass = 'fbButton'
+                    / >
+                      
+
+                    <GoogleLogin
+                      clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+                      buttonText="LOGIN WITH GOOGLE"
+                      // onSuccess={responseGoogle}
+                      // onFailure={responseGoogle}
+                      cookiePolicy={'single_host_origin'}
+                      className = 'goggleButton'
+                      
+                    />
+
+                  <Typography variant = 'caption' sx = {{color : '#0e9f6e',display : 'block', marginTop : '8%'}} >
+                    <Link className = 'links' to = '/login' >Alredy, Have an account. Login  
+                    </Link></Typography>
+                  </Grid>
+
+                </Grid>
+              </Box>
+          </Container>
+  }
+    </>
+
+  )
+}
